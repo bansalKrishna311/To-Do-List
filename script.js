e3d68cae2948fc3afa91889a92851033e817502e
bansalKrishna311 <bansalkrishna311@gmail.com>
2023-12-31 23:22:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const inputBox = document.getElementById("inputBox");
+const listContainer = document.getElementById("list-container");
+function addTask() {
+    if (inputBox.value === '')
+    {
+        alert("Please enter a task");
+
+    }
+    else
+    {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span"); 
+        span.innerHTML = "\u00D7";
+        li.appendChild(span);
+        span.onclick = function()
+        {
+            li.style.display = "none";
+        }
+
+    inputBox.value = '';
+}}
+
+listContainer.addEventListener('click', function(e) {
+
+    if (e.target.tagName === 'LI') {
+      e.target.classList.toggle('checked');
+    }
+    else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+    }
+  }, false);
+
